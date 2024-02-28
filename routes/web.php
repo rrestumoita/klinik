5738ca109ea5ff12945e99b6fcb4a079d21ba6ef
rrestumoita <153410233+rrestumoita@users.noreply.github.com>
2024-02-28 14:53:23 +0700

    menambahkan halaman login

--- routes/web.php
+++ routes/web.php
@@ -16,6 +16,11 @@ use Illuminate\Support\Facades\Route;
 Route::get('/', function () {
     return view('welcome');
 });
+
 Route::get('testing', function () {
     return view('profil');
+});
+
+Route::get('/login', function () {
+    return view('login');
 });
